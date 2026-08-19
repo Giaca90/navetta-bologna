c5c190fbb272b452e81567cc189bfc5486da6be0
Giaca90 <vito.giacalone1@gmail.com>
2026-08-19 22:43:18 +0200

    Update service worker cache name and formatting

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-const CACHE_NAME="navetta-v3";self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["./","./index.html","./manifest.json"]))) });self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+const CACHE_NAME="navetta-v99";
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c=>c.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]))
+  )
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request)
+      .then(r=>r||fetch(e.request))
+  )
+});
+``
