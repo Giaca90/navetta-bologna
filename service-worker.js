13bed32f7558f4542f93e6e50ab7ba2973b0c896
Giaca90 <vito.giacalone1@gmail.com>
2026-08-19 22:53:16 +0200

    Update service worker to new cache version and logic

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,66 @@
-const CACHE_NAME="navetta-v99";
+const CACHE_NAME = "navetta-v100";
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(c=>c.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]))
-  )
+self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll([
+                    "./",
+                    "./index.html",
+                    "./manifest.json",
+                    "./icon-192.png",
+                    "./icon-512.png"
+                ]);
+            })
+    );
+
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request)
-      .then(r=>r||fetch(e.request))
-  )
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+
+                })
+            );
+
+        })
+
+    );
+
+    return self.clients.claim();
+
 });
-``
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+
+            .then(response => {
+
+                return response;
+
+            })
+
+            .catch(() => {
+
+                return caches.match(event.request);
+
+            })
+
+    );
+
+});
